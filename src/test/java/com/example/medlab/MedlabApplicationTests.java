cebee4294749339794c9dc06836ec13a31683a3a
hp <stdgt4@gmail.com>
2024-01-08 22:25:03 +0200

    Generate project structure using Spring Initilizer

---
+++ src/test/java/com/example/medlab/MedlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
