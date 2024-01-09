021ebbe0d780fcad8d0745846deb1d42b1e2a9a0
hp <stdgt4@gmail.com>
2024-01-09 15:27:13 +0200

    Add custom exception for missing resource

---
+++ src/main/java/com/example/medlab/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.medlab.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String message;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
