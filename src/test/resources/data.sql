962f57aba5ea9d7d3f5c04bf9e2996f7e9ea92a6
hp <stdgt4@gmail.com>
2024-01-14 23:34:37 +0200

    Add sql files with the db schema and example input data

---
+++ src/test/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO laboratories (name,address,email,phone,hospital_name)
+VALUES('sAs-Lab1','Sofia Some Random Street 5','alab@somemail.com','+359888888888','Sofia Hospital');
+INSERT INTO laboratories (name,address,email,phone,hospital_name)
+VALUES('sAs-Lab2','Sofia Some Random Street 5','alab@somemail.com','+359888888888','Sofia Hospital');
+INSERT INTO laboratories (name,address,email,phone,hospital_name)
+VALUES('sAs-Lab3','Sofia Some Random Street 5','alab@somemail.com','+359888888888','Sofia Hospital');
+
+--roles
+--INSERT INTO roles (name,description) VALUES('employee','Worker in the lab');
+--INSERT INTO roles (name,description) VALUES('patient','Customer of the lab');
